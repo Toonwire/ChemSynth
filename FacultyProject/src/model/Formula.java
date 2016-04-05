afe18a60ff44990080f7e87c9f1422266b55cfd6
Toonwire <Toonwire@hotmail.com>
2016-04-05 08:51:31 +0200

    Formula parser completed

--- FacultyProject/src/model/Formula.java
+++ FacultyProject/src/model/Formula.java
@@ -2,96 +2,116 @@ package model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Formula {
 
 	private Map<String,Integer> atomMap = new HashMap<String,Integer>();
-	private Map<String,Integer> atomMap2 = new HashMap<String,Integer>();
+	private Map<String,Integer> finalAtomMap = new HashMap<String,Integer>();
 	private ArrayList<String> atomList = new ArrayList<String>();
 	private int amount = 1;
-	private String regex = "[A-Z][a-z]?\\d*|(?<!\\([^)]{0,10})\\(.*\\)\\d+(?![^(]{0,10}\\))";
-//	private String regex = "[A-Z][a-z]?\\d*|\\((?:[^()]{0,10}(?:\\(.*\\))?[^()]{0,10})+\\)\\d+";
+	private final String regex = "[A-Z][a-z]?\\d*|\\((?:[^()]{0,10}(?:\\(.*\\))?[^()]{0,10})+\\)\\d+";
 	
+	private String name;
+	private int coefficient;
 	
-	public Formula(String formula){
+	public Formula(String formula) {
+		this(formula,1);
+	}
+	
+	public Formula(String formula, int coefficient){
+		this.name = formula;
+		this.coefficient = coefficient;
 		
-		findRegex(formula, regex, amount);
-				
+		parseFormula(formula, amount);
+		cleanUpAtomCount();
+		getAtoms();
 		
-		for(Iterator<Entry<String, Integer>> it = atomMap.entrySet().iterator(); it.hasNext(); ) {
-			Entry<String, Integer> entry = it.next();
-		      
-		      if (entry.getKey().matches(".*\\d")){
-		    	  String atom = entry.getKey().split("\\d")[0];
-		    	  int value = Integer.parseInt(entry.getKey().split("[A-Z][a-z]?")[1]);
-		    	  System.out.println("Split " + entry.getKey() + " into " + atom + " and " + value);
-		    	  it.remove();
-		    	  if (atomMap2.containsKey(atom)){
-		    		  value += atomMap2.get(atom);
-		    	  }
-		    	  atomMap2.put(atom, value);
-		    	  
-		      } else{
-		    	  atomMap2.put(entry.getKey(), atomMap.get(entry.getKey()));
-		      }
-		      
-		      
-		}
-		
-		System.out.println("Total amount of single atoms in formula:");
-		for (String str : atomMap2.keySet()){
-			System.out.println(str + "\t" + atomMap2.get(str));
-		}
+	}
+	
+	public Map<String, Integer> getAtoms() {
+//		System.out.println("Total amount of single atoms in formula " + name + ":");
+//		for (String atom : finalAtomMap.keySet()){
+//			System.out.println("--> " + atom + "\t: " + finalAtomMap.get(atom));
+//		}
+		return finalAtomMap;
 		
 	}
 
-	private void findRegex(String strToCheck, String regex, int currentAmount){
+	private void cleanUpAtomCount() {
+		printState();
+		
+		for (String atom : atomMap.keySet()) {
+			String[] a = atom.split("(?<=\\w)(?=\\d)");
+			int coef = atomMap.get(atom);
+			
+			if (a.length > 1) {
+				atom = a[0];
+				coef *= Integer.parseInt(a[1]);
+			} 
+			
+			if (finalAtomMap.containsKey(atom))
+					coef += finalAtomMap.get(atom);
+			
+			finalAtomMap.put(atom, coef);
+		}
+	}
+
+	private void parseFormula(String strToCheck, int currentAmount){
 		Pattern p = Pattern.compile(regex);
 		Matcher m = p.matcher(strToCheck);
 		
-		System.out.println("Matches found by the RegEx over " + strToCheck + ":");
-		System.out.println("Amount: " + amount + "\n" + "New Amount: " + currentAmount +"\n");
-		int i = 1;
+		
+//		System.out.println("\nMatches found by the RegEx over " + strToCheck + ":");
+//		System.out.println("Amount: " + amount + "\n" + "New Amount: " + currentAmount +"\n");
 		while(m.find()){
-//			System.out.println(i + ")\t" + m.group());
-			atomList.add(m.group());
-			if (atomMap.keySet().contains(m.group())){
-				atomMap.put(m.group(), amount + atomMap.get(m.group()));
-				System.out.println("Put " + m.group() + "  : " + amount);
+			String atom = m.group();
+			atomList.add(atom);
+			if (atomMap.keySet().contains(atom)){
+				atomMap.put(m.group(), amount*currentAmount + atomMap.get(atom));
+//				System.out.println("Put " + atom + " \t: " + amount);
+			} else {
+				atomMap.put(atom, amount * currentAmount);
+//				System.out.println("Put " + atom + " \t: " + amount*currentAmount);
 			}
-			atomMap.putIfAbsent(m.group(), amount * currentAmount);
-			System.out.println("Put " + m.group() + "  : " + amount*currentAmount);
-			i++;
 		}
-		this.amount = currentAmount;
-		System.out.println("\n---------\n");
+//		printState();
+//		System.out.println("\n---------\n");
 		
 		for (int j = 0; j < atomList.size(); j++) {
-			System.out.println(j + ") " +atomList.get(j) + "  : " + atomMap.get(atomList.get(j)));
+//			System.out.println(j + ") " +atomList.get(j) + " \t: " + atomMap.get(atomList.get(j)));
 			if (atomList.get(j).contains("(")) {
+				this.amount = atomMap.get(atomList.get(j));
 				String str = atomList.get(j);
 		        atomList.remove(j);
 		        atomMap.remove(str);
-		        System.out.println("removed " + str);
+//		        System.out.println("\nRemoved " + str);
 		        j--;
 		        
-		        int newAmount = Integer.parseInt(str.substring(str.length()-1));
-		        findRegex(str.substring(1, str.length()-2), regex, newAmount);
+		        String[] temp = str.split("(?<=[\\)])(?![A-Za-z])");
+		        int newAmount = Integer.parseInt(temp[temp.length-1]);
+		        parseFormula(str.substring(1, str.length()-temp[temp.length-1].length()-1), newAmount);
 		    }
 		}
-		System.out.println("\n-----");
+//		System.out.println("\n-----");
 	}
 	
-	public ArrayList<String> getatomMap(){
-		return new ArrayList<String>(atomMap.keySet());
+	private void printState() {
+//		System.out.println("\nState of AtomMap:");
+//		for (String atom : atomMap.keySet()) {
+//			System.out.println("--> " + atom + "\t: " + atomMap.get(atom));
+//		}
+//		System.out.println();
+		
 	}
 	
-	public Map<String,Integer> getAtomMap(){
-		return atomMap;
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getCoefficient() {
+		return this.coefficient;
 	}
 }
